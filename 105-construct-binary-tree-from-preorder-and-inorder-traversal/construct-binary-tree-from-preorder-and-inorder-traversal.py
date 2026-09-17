17c13b2964faebcaa4b3f101e4dc306108824b7a
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-17 20:23:22 +0530

    Remove initial implementation of buildTree method

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,6 +1,4 @@
-class Solution:
-    def buildTree(self, preorder: list[int], inorder: list[int]) -> Optional[TreeNode]:
-        in_map = {val: i for i, val in enumerate(inorder)}
+
        
          
             
