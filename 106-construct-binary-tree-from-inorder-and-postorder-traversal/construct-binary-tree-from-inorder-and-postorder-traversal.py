770977c60da22e89b440c81b676d06b0e24f76de
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-17 20:21:25 +0530

    Remove initial buildTree method implementation
    Removed the initial implementation of the buildTree method.

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -1,9 +1,2 @@
-class Solution:
-    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        # Map value to its index in inorder array for O(1) lookups
-        in_map = {val: i for i, val in enumerate(inorder)}
-        
-        # Pointer for the current root in postorder array
-        post_idx = len(postorder) - 1
-   
+
            
