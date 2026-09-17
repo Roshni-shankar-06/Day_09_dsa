2e19f6950e55c2e4e6f8e3a08f5e5dd3fffdcd9b
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-17 20:12:14 +0530

    Remove incomplete implementation of largestRectangleArea
    Removed incomplete code for largestRectangleArea function.

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -2,8 +2,4 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int ans = 0;
-        stack<int> stack;
-        for (int i = 0; i <= heights.size(); ++i) {
-            while (!stack.empty() && (i == heights.size() || heights[stack.top()] > heights[i])) {
-                const int 
-      
+     
