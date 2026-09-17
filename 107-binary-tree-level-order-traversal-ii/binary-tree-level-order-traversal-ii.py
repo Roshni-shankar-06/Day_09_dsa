ba7f621edf477207e7d5cdb84a47c2fd1175d01e
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-17 20:11:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (73.45%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,30 @@
+from collections import deque
+from typing import List, Optional
+
+
+class Solution:
+
+  def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+    if not root:
+      return []
+
+    result = []
+    queue = deque([root])
+
+    while queue:
+      level_size = len(queue)
+      current_level = []
+
+      for _ in range(level_size):
+        node = queue.popleft()
+        current_level.append(node.val)
+
+        if node.left:
+          queue.append(node.left)
+        if node.right:
+          queue.append(node.right)
+
+      result.append(current_level)
+
+    # Reverse to get bottom-up order
+    return result[::-1]
