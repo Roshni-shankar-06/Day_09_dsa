0ffb4358d8d361cc90240d22adc08c3d9eb808bc
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-17 20:23:11 +0530

    Clean up buildTree method in Solution class
    Removed unused variables and function definitions.

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,11 +1,7 @@
 class Solution:
     def buildTree(self, preorder: list[int], inorder: list[int]) -> Optional[TreeNode]:
         in_map = {val: i for i, val in enumerate(inorder)}
-        self.pre_idx = 0
-        
-        def helper(in_left: int, in_right: int) -> Optional[TreeNode]:
-           
-            
+       
          
             
        
