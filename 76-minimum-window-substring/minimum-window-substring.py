e1e91118d56e63b280a0771d6afc50cf3c7533a1
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-17 20:14:38 +0530

    Remove initial implementation of minWindow function

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,16 +1,2 @@
-from collections import Counter
 
-class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        if not s or not t or len(s) < len(t):
-            return ""
-        
-        # Frequency map for characters in t
-        t_count = Counter(t)
-        # Dictionary to keep track of characters in the current window
-        window_count = {}
-        
-        # 'required' is the number of unique characters in t that must be present in the window
-        # 'formed' tracks how many unique characters meet their target frequency requirement
-      
   
