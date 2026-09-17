e5cd7f7b068b51161e40ddb09ec63958fe1c6d52
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-17 20:18:58 +0530

    Remove Solution class and levelOrderBottom method
    Removed the Solution class and its levelOrderBottom method.

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
@@ -2,11 +2,6 @@ from collections import deque
 from typing import List, Optional
 
 
-class Solution:
-
-  def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
-    if not root:
-      return []
 
 
      
