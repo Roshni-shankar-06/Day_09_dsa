d7cbf05d48b81d256c819112d180fe49ba91de76
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-17 20:16:20 +0530

    Clean up unused variables in the algorithm
    Removed unused variables related to window sum and minimum length.

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -4,13 +4,7 @@ class Solution:
         INF = n + 1
         best = [INF] * n
         left = 0
-        window_sum = 0
-        min_len = INF
-        result = INF
-        
-        for right in range(n):
-            window_sum += arr[right]
-            
+  
           
             
            
